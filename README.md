87788cff0880f944a5a9b90719081d39f29c3eb4
Lisa Dean <lisa@lisadean.net>
2019-02-24 18:11:41 -0500

    Added most obvious files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+
+Inspiration (among others):
+https://github.com/holman/dotfiles
+https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789
---
+++ install_dotfiles.sh
@@ -0,0 +1,6 @@
+DOTFILES_DIR=~/dotfiles
+
+ln -svf "${DOTFILES_DIR}/.bash_profile" ~
+ln -svf "${DOTFILES_DIR}/.aliases" ~
+ln -svf "${DOTFILES_DIR}/.gitconfig" ~
+ln -svf "${DOTFILES_DIR}/.gitignore_global" ~
