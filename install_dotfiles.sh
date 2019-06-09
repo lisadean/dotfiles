1f1165fc37965d5e577206109ba69a9f5e2bf467
Lisa Dean <lisa@lisadean.net>
2019-06-09 11:27:10 -0400

    Moved prompt and paths to their own files. Changed Android path

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,3 +4,5 @@ ln -svf "${DOTFILES_DIR}/.bash_profile" ~
 ln -svf "${DOTFILES_DIR}/.aliases" ~
 ln -svf "${DOTFILES_DIR}/.gitconfig" ~
 ln -svf "${DOTFILES_DIR}/.gitignore_global" ~
+ln -svf "${DOTFILES_DIR}/.prompt" ~
+ln -svf "${DOTFILES_DIR}/.paths" ~
