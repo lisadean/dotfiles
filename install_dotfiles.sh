8e776c37de5f7522f88f3e50bf7474803cc468aa
Lisa Dean <lisa@lisadean.net>
2020-11-14 17:05:42 -0500

    unsaved files

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,7 +1,8 @@
-DOTFILES_DIR=~/dotfiles-supply
+DOTFILES_DIR=~/dotfiles
 
 ln -svf "${DOTFILES_DIR}/.bash_profile" ~
 ln -svf "${DOTFILES_DIR}/.aliases" ~
+ln -svf "${DOTFILES_DIR}/.functions" ~
 ln -svf "${DOTFILES_DIR}/.gitconfig" ~
 ln -svf "${DOTFILES_DIR}/.gitignore_global" ~
 ln -svf "${DOTFILES_DIR}/.prompt" ~
