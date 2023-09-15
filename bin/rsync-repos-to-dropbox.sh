8ac68a71ccf8385422c2cd88bd659f85fd014e92
Lisa Dean <lisa@lisadean.net>
2023-09-15 02:54:03 -0400

    move to new repo

--- bin/rsync-repos-to-dropbox.sh
+++
@@ -1,1 +0,0 @@
-rsync --archive --verbose --exclude=.git --exclude=.gitignore --exclude=__pycache__ --exclude=node_modules ~/git-repos/ ~/Dropbox/git-repos-rsync
--- bin/start_firefox_with_ssl_export.sh
+++
@@ -1,2 +0,0 @@
-export SSLKEYLOGFILE=$HOME/Desktop/keylogfile.txt
-open -a firefox
